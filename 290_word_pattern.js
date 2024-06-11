f2f45d01aa4da1f0c0b8e3d17867a7707ba3472d
Eray <solenkoleray@gmail.com>
2024-06-11 21:31:35 +0300

    290_word_pattern solved

---
+++ 290_word_pattern.js
@@ -0,0 +1,72 @@
+/* 
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
+*/
+
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let words = s.split(" ")
+
+    if (pattern.length != words.length) {
+        return false;
+    }
+    if(new Set(words).size != new Set(pattern).size) {
+        return false
+    }
+        
+    let patternMap = {}
+    
+    let isMatches = true
+
+    for (let i = 0; i < pattern.length ; i++) {
+        if (pattern[i] in patternMap) {
+            if (patternMap[pattern[i]] != words[i]) {
+                isMatches = false
+                break
+            }
+        } else {
+            patternMap[pattern[i]] = words[i]
+        }
+    }
+
+    return isMatches
+};
+
+
+// Test cases
+
+console.log(wordPattern("abba", "dog cat cat dog")) // true
+console.log(wordPattern("abba", "dog cat cat fish")) // false
+console.log(wordPattern("aaaa", "dog cat cat dog")) // false
+console.log(wordPattern("abba", "dog dog dog dog")) // false
+console.log(wordPattern("abbc", "dog cat cat fish")) // true
+console.log(wordPattern("abbc", "dog cat cat cat")) // false
